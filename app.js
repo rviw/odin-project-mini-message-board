95ff812483321c2d9f58521e5b443696866017f5
rviw <216364371+rviw@users.noreply.github.com>
2026-03-19 15:14:41 +0900

    feat: handle new message submissions

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const indexRouter = require("./routes/indexRouter");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
