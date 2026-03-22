76f72e8cf7aac135c88069942d90de98347e907a
rviw <216364371+rviw@users.noreply.github.com>
2026-03-22 10:21:29 +0900

    feat: add global stylesheet

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const indexRouter = require("./routes/indexRouter");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 
